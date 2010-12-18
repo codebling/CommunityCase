92e084398c10887ca3cb359cacd02d7e1a66f736
worsecodes <worsecodes@gmail.com>
2010-12-18 17:39:20 -0500

    +Added VersionTree

--- src/org/community/intellij/plugins/communitycase/actions/VersionTree.java
+++ src/org/community/intellij/plugins/communitycase/actions/VersionTree.java
@@ -15,6 +15,7 @@
  */
 package org.community.intellij.plugins.communitycase.actions;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.FileStatusManager;
@@ -23,7 +24,11 @@ import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vcs.changes.ui.RollbackChangesDialog;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.vcsUtil.VcsUtil;
+import org.community.intellij.plugins.communitycase.Util;
 import org.community.intellij.plugins.communitycase.Vcs;
+import org.community.intellij.plugins.communitycase.commands.Command;
+import org.community.intellij.plugins.communitycase.commands.SimpleHandler;
 import org.community.intellij.plugins.communitycase.i18n.Bundle;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,17 +39,23 @@ import java.util.List;
  * "revert" action
  */
 public class VersionTree extends BasicAction {
+  private static Logger log=Logger.getInstance(VersionTree.class.getName());
 
   @Override
   public boolean perform(@NotNull final Project project, Vcs vcs, @NotNull final List<VcsException> exceptions, @NotNull VirtualFile[] affectedFiles) {
-    final List<Change> changes = new ArrayList<Change>(affectedFiles.length);
-    for (VirtualFile f : affectedFiles) {
-      Change ch = ChangeListManager.getInstance(project).getChange(f);
-      if (ch != null) {
-        changes.add(ch);
+    VirtualFile root;
+    for(VirtualFile vf:affectedFiles) {
+      try {
+        root=Util.getRoot(vf);
+        root=root.getParent();
+        SimpleHandler handler=new SimpleHandler(project,root,Command.VERSION_TREE_GRAPHICAL);
+        handler.endOptions();
+        handler.addParameters(vf.getName());
+        handler.run();
+      } catch(VcsException e) {
+        log.error(e);
       }
     }
-    RollbackChangesDialog.rollbackChanges(project, changes);
     return false;
   }
 
@@ -56,10 +67,11 @@ public class VersionTree extends BasicAction {
 
   @Override
   protected boolean isEnabled(@NotNull Project project, @NotNull Vcs vcs, @NotNull VirtualFile... vFiles) {
-    for (VirtualFile file : vFiles) {
+/*    for (VirtualFile file : vFiles) {
       FileStatus status = FileStatusManager.getInstance(project).getStatus(file);
       if (status == FileStatus.UNKNOWN || status == FileStatus.NOT_CHANGED) return false;
     }
+*/
     return true;
   }
 }
