489ca1045270ec2acce5edf4e1540683521644de
worsecodes <worsecodes@gmail.com>
2011-01-25 00:54:22 -0500

    *make checkouts instantaneous by hijacking first

--- src/org/community/intellij/plugins/communitycase/edit/EditFileProvider.java
+++ src/org/community/intellij/plugins/communitycase/edit/EditFileProvider.java
@@ -1,10 +1,13 @@
 package org.community.intellij.plugins.communitycase.edit;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vcs.FilePathImpl;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.community.intellij.plugins.communitycase.Util;
 import org.community.intellij.plugins.communitycase.commands.Command;
+import org.community.intellij.plugins.communitycase.commands.LineHandler;
 import org.community.intellij.plugins.communitycase.commands.SimpleHandler;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,16 +29,21 @@ public class EditFileProvider implements com.intellij.openapi.vcs.EditFileProvid
     //just use the first file's parent as execution dir (other files will be relative to this one)
     VirtualFile execDir=Util.getRoot(virtualFiles[0]).getParent();
 
-    SimpleHandler handler=new SimpleHandler(myProject, execDir, Command.CHECKOUT);
+    //todo wc when converting Commands to Handlers, include file.refresh(false, false) (see VcsHandleType.java:50)
+    //  also check for and report errors such as when file is checked out reserved by another user
+    LineHandler handler=new LineHandler(myProject, execDir, Command.CHECKOUT);
     handler.setSilent(false);
+    handler.setStdoutSuppressed(false);
     handler.addParameters("-res");//reserved    //todo wc read from settings whether to reserve or unreserve (–unr)
     handler.addParameters("-nc");//no comment   //todo wc optionally prompt for this
+    handler.addParameters("-use");//use hijack - force this so that we can clear the read-only flag ahead of command execution
     handler.endOptions();
     for(VirtualFile file:virtualFiles) {
       handler.addParameters(Util.getRelativeFilePath(file, execDir)); //make all other files relative to the exec dir we chose
+      new FilePathImpl(file).getIOFile().setWritable(true);
     }
 
-    handler.run();
+    handler.start();
   }
 
   @Override
