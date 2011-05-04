2d17441830cde2a10d5ff461b2c3eb2a153e7b38
worsecodes <worsecodes@gmail.com>
2011-05-04 15:52:12 -0400

    *renamed old Checkout action to CheckoutBranch (since that's what it did) +added new Checkout action which checks out a file

--- src/org/community/intellij/plugins/communitycase/actions/Checkout.java
+++ src/org/community/intellij/plugins/communitycase/actions/Checkout.java
@@ -1,17 +1,87 @@
 package org.community.intellij.plugins.communitycase.actions;
 
-/**
- * TODO ascher 5/2/11 MISSING_CLASS_HEADER_JAVADOC
- * <p/>
- * <p>
- * The information contained herein is the property of OZ Communications Inc. and is strictly proprietary and
- * confidential. Except as expressly authorized in writing by OZ Communications Inc., you do not have the right
- * to use the information contained herein and you shall not disclose it to any third party.</p>
- * <p/>
- * <br>Copyright 2011 OZ Communications Inc. All Rights Reserved.<br>
- *
- * @author Ben Ascher
- * @since 5/2/11
- */
-public class Checkout {
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.FileStatus;
+import com.intellij.openapi.vcs.FileStatusManager;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.community.intellij.plugins.communitycase.Vcs;
+import org.community.intellij.plugins.communitycase.edit.EditFileProvider;
+import org.community.intellij.plugins.communitycase.i18n.Bundle;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Checkout extends BasicAction {
+
+  private static final Logger log=Logger.getInstance("#"+Checkout.class.getName());
+  private static final String NAME=Bundle.getString("checkout.action.name");
+
+  public Checkout() {
+    super(NAME);
+  }
+
+  /**
+   * Perform the action over set of files
+   *
+   * @param project       the context project
+   * @param mksVcs        the vcs instance
+   * @param exceptions    the list of exceptions to be collected.
+   * @param files the files to be affected by the operation
+   * @return true if the operation scheduled a background job, or cleanup is not needed
+   */
+  @Override
+  protected boolean perform(@NotNull Project project,
+                            Vcs mksVcs,
+                            @NotNull List<VcsException> exceptions,
+                            @NotNull VirtualFile[] files) {
+    try {
+      new EditFileProvider(project).editFiles(checkIfEnabled(project,mksVcs,files).toArray(new VirtualFile[0]));
+    } catch(VcsException e) {
+      //log.error(Bundle.getString("checkout.action.error"),e);
+      exceptions.add(e);
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * @return the name of action (it is used in a number of ui elements)
+   */
+  @NotNull
+  @Override
+  protected String getActionName() {
+    return NAME;
+  }
+
+  /**
+   * Check if the action should be enabled for the set of the fils
+   *
+   * @param project the context project
+   * @param vcs     the vcs to use
+   * @param vFiles  the set of files
+   * @return true if the action should be enabled
+   */
+  @Override
+  protected boolean isEnabled(@NotNull Project project, @NotNull Vcs vcs, @NotNull VirtualFile... vFiles) {
+    return checkIfEnabled(project,vcs,vFiles).size() >= 1;
+  }
+
+  private Collection<VirtualFile> checkIfEnabled(@NotNull Project project,
+                                                 @NotNull Vcs vcs,
+                                                 @NotNull VirtualFile... vFiles) {
+    Collection<VirtualFile> enabled=new ArrayList<VirtualFile>();
+    for(VirtualFile file:vFiles) {
+      FileStatus fileStatus=FileStatusManager.getInstance(project).getStatus(file);
+      if(fileStatus!=FileStatus.IGNORED
+          && fileStatus!=FileStatus.UNKNOWN
+          && fileStatus!=FileStatus.MODIFIED)
+        enabled.add(file);
+    }
+    return enabled;
+  }
 }
